c7c553ac0f11721d91c466cd5fdf221cf0797866
Pravin Bhat <bhatpravin@gmail.com>
2024-06-25 12:59:04 -0400

    Updated Cassandra/DSE script to use user provided CL or default to ALL

--- json_etl_dsbulk_cass.py
+++ json_etl_dsbulk_cass.py
@@ -11,6 +11,15 @@ import json
 import sys
 import os
 
+class markup:
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
 def transfromJson(json_ip_file_path, json_op_file_path):
     with open(json_ip_file_path, 'r') as fi:
         data = json.load(fi)
@@ -18,9 +27,9 @@ def transfromJson(json_ip_file_path, json_op_file_path):
     with open(json_op_file_path, 'w') as fo:
         for i in data:
             fo.write('{"row_id":"' + i['id'] + '","attributes_blob":"' + '","body_blob":"' + i['page_content'] 
-                    + '","metadata_s":' + json.dumps(i['metadata']) + ',"vector":' + json.dumps(i['vector']) + '}\n')
+                + '","metadata_s":' + json.dumps(i['metadata']) + ',"vector":' + json.dumps(i['vector']) + '}\n')
 
-def loadData(pathToDSBulk, host, port, username, password, ks, table, truststore_path, truststore_password, output_json_file_path):
+def loadData(pathToDSBulk, host, port, username, password, ks, table, truststore_path, truststore_password, output_json_file_path, cl):
     os.system(pathToDSBulk + 
               """ load -c json \
               -h {host} -port {port} \
@@ -33,10 +42,10 @@ def loadData(pathToDSBulk, host, port, username, password, ks, table, truststore
               --datastax-java-driver.advanced.ssl-engine-factory.truststore-password {truststore_password} \
               --datastax-java-driver.advanced.ssl-engine-factory.hostname-validation false \
               --driver.advanced.auth-provider.class DsePlainTextAuthProvider \
-              -cl LOCAL_QUORUM -url {output_json_file_path} """
+              -cl {cl} -url {output_json_file_path} """
               .format(host=host, port=port, username=username, password=password, ks=ks, table=table, 
                       truststore_path=truststore_path, truststore_password=truststore_password, 
-                       output_json_file_path=output_json_file_path))
+                       output_json_file_path=output_json_file_path, cl=cl))
 
 def main():
     try:
@@ -53,13 +62,19 @@ def main():
         table = sys.argv[8]
         truststore_path = sys.argv[9]
         truststore_password = sys.argv[10]
-        loadData(pathToDSBulk, host, port, username, password, ks, table, truststore_path, truststore_password, json_op_file_path)
-        print(f"Successfully loaded!!")
+        if len(sys.argv) > 11:
+            cl = sys.argv[11].upper()
+            print(f"{markup.OKBLUE}Using provided consistency level: {cl} for loading data via DSBulk!{markup.ENDC}")
+        else:
+            cl = "ALL"
+            print(f"{markup.WARNING}{markup.BOLD}{markup.UNDERLINE}Using consistency level: {cl} for loading data via DSBulk, as no value provided via command line.{markup.ENDC}")
+        loadData(pathToDSBulk, host, port, username, password, ks, table, truststore_path, truststore_password, json_op_file_path, cl)
+        print(f"{markup.OKGREEN}Data loaded successfully via DSBulk, see logs for details!!{markup.ENDC}")
 
     except Exception as e:
         print(f"An error occurred: {e}", file=sys.stderr)
-        print(f"Usage: python3 json_etl_dsbulk.py <path-to-input-json-file> <path-to-DSBulk> <host> <port>" + 
-              "<username> <password> <keyspace> <table> <truststore_path> <truststore_password>", file=sys.stderr)
+        print(f"{markup.FAIL}Usage: python3 json_etl_dsbulk.py <path-to-input-json-file> <path-to-DSBulk> <host> <port> <username>{markup.ENDC}" + 
+              f"{markup.FAIL} <password> <keyspace> <table> <truststore_path> <truststore_password> <consistency-level>{markup.ENDC}", file=sys.stderr)
 
 if __name__ == "__main__":
     main()
